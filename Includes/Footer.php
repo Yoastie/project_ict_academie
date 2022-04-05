9cc31950ab77f7749a5d051fbadc6c043cdb0865
Sepdegraaff <sep.degraaff@edu-kw1c.nl>
2022-04-05 11:17:02 +0200

    Lars

---
+++ Includes/Footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Sep de Graaff
+ * Date: 5-4-2022
+ * File: Footer.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<body>
+<header>
+    <nav>
+        <ul>
+
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
+
+
