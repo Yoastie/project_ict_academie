b8ce199747550faae4a73a90d0b66fef46a60a7c
Sepdegraaff <sep.degraaff@edu-kw1c.nl>
2022-04-07 15:08:33 +0200

    Project

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -8,7 +8,8 @@
 <Footer>
     <div class="lijn"></div>
     <div id="facebookFoto"><img src="./Images/Facebook.png" alt="Facebook"></div>
-    <div id="twitterFoto"><img src="./Images/Twitter.png" alt="Facebook"></div>
+    <div id="twitterFoto"><img src="./Images/Twitter.png" alt="twitter"></div>
+    <div id="instagramFoto"><img src="./Images/Instagram.jpg" alt="instagram"></div>
     <p>
         Privacyverklaring
     </p>
