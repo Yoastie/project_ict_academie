cf227dec728d4658639be4b24920642f3bf013d3
Administrator <Lars.vaniersel@edu-kw1c.nl>
2022-04-12 09:58:14 +0200

    u[date

---
+++ Includes/header.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!doctype html>
+
+<!--
+    Auteur:       Lars van Iersel
+    Aanmaakdatum: 7-4-2021
+    Omschrijving: project
+ -->
+
+<html>
+<head>
+    <title> KW1C </title>
+    <!-- Decleratie tekenset -->
+    <meta charset="UTF-8">
+    <!-- Decleratie viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--link naar ccs bestand-->
+    <link rel="stylesheet" href="/styles/header.css">
+
+</head>
+<body>
+<header>
+    <!-- de navbar id en die wordt aangesproken vanaf javascript -->
+    <nav>
+        <ul id="navbar">
+            <a href="#">
+                <li>opleidingen</li>
+            </a>
+            <a href="#">
+                <li>studiekeuze</li>
+            </a>
+            <a href="#">
+                <li>volwassenen & werkgevers</li>
+            </a>
+            <a href="#">
+                <li>studenten info</li>
+            </a>
+        </ul>
+    </nav>
+</header>
+<main>
+    <!-- de fotos op de navbar -->
+    <a href="#"><img src="../Images/download.jpg" alt="kw1c logo" id="achtergrondfoto"></a>
+    <a href="#"><img src="../Images/slot.png" alt="slot img" id="slot"></a>
+    <a href="#"><img src="../Images/nederlandse-vlag.png" alt="vlag" id="vlag"></a>
+
