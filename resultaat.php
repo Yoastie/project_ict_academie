2af1d017a561b5c8f944b6014034e29bc1e3a9da
MildlyUpsetGoose <sven.vangasteren@edu-kw1c.nl>
2022-04-15 14:08:52 +0200

    project

--- resultaat.php
+++ resultaat.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <?php
 /**
  * User: Sep de Graaff, Lars van Iersel, Sven van Gasteren
@@ -5,3 +6,28 @@
  * File: resultaat.php
  */
 ?>
+
+<html lang="en">
+<head>
+    <title>
+        Resultaten!
+    </title>
+    <!-- link naar CSS bestand -->
+    <link rel="stylesheet" href="./styles/resultaat.css">
+    <script src="" defer></script>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <p id="volledigeNaam">
+        Hallo, <?php echo ($_POST['firstname']); ?> <?php echo ($_POST['lastname']); ?>
+        <br>
+        Hier zijn uw resulaten van de enquete die u net heeft ingevuld:
+    </p>
+
+    <p id="resultaten">
+        
+    </p>
+</body>
+</html>
