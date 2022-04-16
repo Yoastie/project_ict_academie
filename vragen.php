638994545eb0b301c5cc7a47722da97f81ef5ba0
MildlyUpsetGoose <sven.vangasteren@edu-kw1c.nl>
2022-04-16 10:54:22 +0200

    project

--- vragen.php
+++ vragen.php
@@ -30,78 +30,78 @@ include "Includes/header.php"
             value 5 = hardware-->
             <p>Vind je het leuk om moeilijke opdrachten op te lossen?</p>
         <!--    radio voegt de vraag toe aan een groep zoals question1 dan kan je maar 1 ding aanvinken bij question 1-->
-            <input type="radio" name="question1" value="1"> ja
-            <input type="radio" name="question1" value="5"> nee
+            <input type="radio" name="question1" value="0"> ja
+            <input type="radio" name="question1" value="40"> nee
 
             <p>Lijkt het je leuk om te coderen?</p>
 
-            <input type="radio" name="question2" value="1"> ja
-            <input type="radio" name="question2" value="5"> nee
+            <input type="radio" name="question2" value="0"> ja
+            <input type="radio" name="question2" value="40"> nee
 
             <p>Lijkt het je leuk om computers te bouwen en daar meer over te leren?</p>
 
-            <input type="radio" name="question3" value="5"> ja
-            <input type="radio" name="question3" value="1"> nee
+            <input type="radio" name="question3" value="0"> ja
+            <input type="radio" name="question3" value="40"> nee
 
             <p>Kan je 8 uur lang achter een computer aan het werk zijn? </p>
 
-            <input type="radio" name="question4" value="1"> ja
-            <input type="radio" name="question4" value="5"> nee
+            <input type="radio" name="question4" value="0"> ja
+            <input type="radio" name="question4" value="40"> nee
 
             <p>Beheers je al 1 of meerdere codeer talen?</p>
 
-            <input type="radio" name="question5" value="1"> ja
-            <input type="radio" name="question5" value="5"> nee
+            <input type="radio" name="question5" value="0"> ja
+            <input type="radio" name="question5" value="40"> nee
 
             <p>Heb je al een keer een computer gebouwd?</p>
 
-            <input type="radio" name="question6" value="5"> ja
-            <input type="radio" name="question6" value="1"> nee
+            <input type="radio" name="question6" value="40"> ja
+            <input type="radio" name="question6" value="0"> nee
 
             <p>Zou je meer over de systemen willen leren waarop computers draaien?</p>
 
-            <input type="radio" name="question7" value="1"> ja
-            <input type="radio" name="question7" value="5"> nee
+            <input type="radio" name="question7" value="0"> ja
+            <input type="radio" name="question7" value="40"> nee
 
             <p>Ben je iemand die snel afgeleid is?</p>
 
-            <input type="radio" name="question8" value="1"> ja
-            <input type="radio" name="question8" value="5"> nee
+            <input type="radio" name="question8" value="0"> ja
+            <input type="radio" name="question8" value="40"> nee
 
             <p>vind je het erg om veel te googelen voor school als je iets niet begrijpt?</p>
 
-            <input type="radio" name="question9" value="5"> ja
-            <input type="radio" name="question9" value="1"> nee
+            <input type="radio" name="question9" value="40"> ja
+            <input type="radio" name="question9" value="0"> nee
 
             <p></p>
 
-            <input type="radio" name="question10" value="1"> ja
-            <input type="radio" name="question10" value="5"> nee
+            <input type="radio" name="question10" value="0"> ja
+            <input type="radio" name="question10" value="40"> nee
 
             <p></p>
 
-            <input type="radio" name="question11" value="1"> ja
-            <input type="radio" name="question11" value="5"> nee
+            <input type="radio" name="question11" value="0"> ja
+            <input type="radio" name="question11" value="40"> nee
 
             <p></p>
 
-            <input type="radio" name="question12" value="1"> ja
-            <input type="radio" name="question12" value="5"> nee
+            <input type="radio" name="question12" value="0"> ja
+            <input type="radio" name="question12" value="40"> nee
 
             <p></p>
 
-            <input type="radio" name="question13" value="1"> ja
-            <input type="radio" name="question13" value="5"> nee
+            <input type="radio" name="question13" value="0"> ja
+            <input type="radio" name="question13" value="40"> nee
 
             <p></p>
 
-            <input type="radio" name="question14" value="1"> ja
-            <input type="radio" name="question14" value="5"> nee
+            <input type="radio" name="question14" value="0"> ja
+            <input type="radio" name="question14" value="40"> nee
 
             <p></p>
 
-            <input type="radio" name="question15" value="1"> ja
-            <input type="radio" name="question15" value="5"> nee
+            <input type="radio" name="question15" value="0"> ja
+            <input type="radio" name="question15" value="40"> nee
             <br>
             <br>
         <!--    deze knop zorgt dat je alle data naar de resultaten pagina brengt-->
