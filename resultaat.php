0e465180569e143e619774bcf20655db0ef90bdb
MildlyUpsetGoose <sven.vangasteren@edu-kw1c.nl>
2022-04-16 10:31:38 +0200

    project

--- resultaat.php
+++ resultaat.php
@@ -8,26 +8,26 @@
 ?>
 
 <html lang="en">
-<head>
-    <title>
-        Resultaten!
-    </title>
-    <!-- link naar CSS bestand -->
-    <link rel="stylesheet" href="./styles/resultaat.css">
-    <script src="" defer></script>
-    <meta charset = "UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+    <head>
+        <title>
+            Resultaten!
+        </title>
+        <!-- link naar CSS bestand -->
+        <link rel="stylesheet" href="./styles/resultaat.css">
+        <script src="" defer></script>
+        <meta charset = "UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
 
-<body>
-    <p id="volledigeNaam">
-        Hallo, <?php echo ($_POST['firstname']); ?> <?php echo ($_POST['lastname']); ?>
-        <br>
-        Hier zijn uw resulaten van de enquete die u net heeft ingevuld:
-    </p>
+    <body>
+        <p id="volledigeNaam">
+            Hallo, <?php echo ($_POST['firstname']); ?> <?php echo ($_POST['lastname']); ?>
+            <br>
+            Hier zijn uw resulaten van de enquete die u net heeft ingevuld:
+        </p>
 
-    <p id="resultaten">
-        
-    </p>
-</body>
+        <p id="resultaten">
+
+        </p>
+    </body>
 </html>
