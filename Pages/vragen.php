55e907ad3b93f9b4343ce7d78f3034c1746eaf05
MildlyUpsetGoose <sven.vangasteren@edu-kw1c.nl>
2023-04-10 10:41:40 +0200

    Project T3

--- Pages/vragen.php
+++ Pages/vragen.php
@@ -29,82 +29,82 @@ include "../Includes/header.php"
         <!-- value 0 = software,
         value 40 = hardware-->
         <p>Vind je het leuk om moeilijke opdrachten op te lossen?</p>
-    <!--    radio voegt de vraag toe aan een groep zoals question1 dan kan je maar 1 ding aanvinken bij question 1-->
-        <input type="radio" name="question1" value="0"> ja
-        <input type="radio" name="question1" value="40"> nee
+        <!--    radio voegt de vraag toe aan een groep zoals question1 dan kan je maar 1 ding aanvinken bij question 1-->
+        <input type="radio" name="question1" value="0" required="required"> ja
+        <input type="radio" name="question1" value="40" required="required"> nee
 
         <p>Lijkt het je leuk om te coderen?</p>
 
-        <input type="radio" name="question2" value="0"> ja
-        <input type="radio" name="question2" value="40"> nee
+        <input type="radio" name="question2" value="0" required="required"> ja
+        <input type="radio" name="question2" value="40" required="required"> nee
 
         <p>Lijkt het je leuk om computers te bouwen en daar meer over te leren?</p>
 
-        <input type="radio" name="question3" value="0"> ja
-        <input type="radio" name="question3" value="40"> nee
+        <input type="radio" name="question3" value="0" required="required"> ja
+        <input type="radio" name="question3" value="40" required="required"> nee
 
         <p>Kan je 8 uur lang achter een computer aan het werk zijn? </p>
 
-        <input type="radio" name="question4" value="0"> ja
-        <input type="radio" name="question4" value="40"> nee
+        <input type="radio" name="question4" value="0" required="required"> ja
+        <input type="radio" name="question4" value="40" required="required"> nee
 
         <p>Beheers je al 1 of meerdere codeer talen?</p>
 
-        <input type="radio" name="question5" value="0"> ja
-        <input type="radio" name="question5" value="40"> nee
+        <input type="radio" name="question5" value="0" required="required"> ja
+        <input type="radio" name="question5" value="40" required="required"> nee
 
         <p>Heb je al een keer een computer gebouwd?</p>
 
-        <input type="radio" name="question6" value="40"> ja
-        <input type="radio" name="question6" value="0"> nee
+        <input type="radio" name="question6" value="40" required="required"> ja
+        <input type="radio" name="question6" value="0" required="required"> nee
 
         <p>Zou je meer over de systemen willen leren waarop computers draaien?</p>
 
-        <input type="radio" name="question7" value="0"> ja
-        <input type="radio" name="question7" value="40"> nee
+        <input type="radio" name="question7" value="0" required="required"> ja
+        <input type="radio" name="question7" value="40" required="required"> nee
 
         <p>Ben je iemand die snel afgeleid is?</p>
 
-        <input type="radio" name="question8" value="0"> ja
-        <input type="radio" name="question8" value="40"> nee
+        <input type="radio" name="question8" value="0" required="required"> ja
+        <input type="radio" name="question8" value="40" required="required"> nee
 
         <p>vind je het erg om veel te googelen voor school als je iets niet begrijpt?</p>
 
-        <input type="radio" name="question9" value="40"> ja
-        <input type="radio" name="question9" value="0"> nee
+        <input type="radio" name="question9" value="40" required="required"> ja
+        <input type="radio" name="question9" value="0" required="required"> nee
 
         <p>kun je goed met je handen werken?</p>
 
-        <input type="radio" name="question10" value="40"> ja
-        <input type="radio" name="question10" value="0"> nee
+        <input type="radio" name="question10" value="40" required="required"> ja
+        <input type="radio" name="question10" value="0" required="required"> nee
 
         <p>kun je goed in een team werken?</p>
 
-        <input type="radio" name="question11" value="0"> ja
-        <input type="radio" name="question11" value="40"> nee
+        <input type="radio" name="question11" value="0" required="required"> ja
+        <input type="radio" name="question11" value="40" required="required"> nee
 
         <p>had je op de middelbare school ict als een vak en vond je daar de hardware kant leuk?</p>
 
-        <input type="radio" name="question12" value="40"> ja
-        <input type="radio" name="question12" value="0"> nee
+        <input type="radio" name="question12" value="40" required="required"> ja
+        <input type="radio" name="question12" value="0" required="required"> nee
 
         <p>had je op de middelbare school ict als een vak en vond je daar de software kant leuk?</p>
 
-        <input type="radio" name="question13" value="0"> ja
-        <input type="radio" name="question13" value="40"> nee
+        <input type="radio" name="question13" value="0" required="required"> ja
+        <input type="radio" name="question13" value="40" required="required"> nee
 
         <p>heb je al eerder een software gerelateerde opleiding gevolgd? </p>
 
-        <input type="radio" name="question14" value="0"> ja
-        <input type="radio" name="question14" value="40"> nee
+        <input type="radio" name="question14" value="0" required="required"> ja
+        <input type="radio" name="question14" value="40" required="required"> nee
 
         <p>vind je het leuk om te leren om met verschillende apparatuur te werken?</p>
 
-        <input type="radio" name="question15" value="0"> ja
-        <input type="radio" name="question15" value="40"> nee
+        <input type="radio" name="question15" value="0" required="required"> ja
+        <input type="radio" name="question15" value="40" required="required"> nee
         <br>
         <br>
-    <!--    deze knop zorgt dat je alle data naar de resultaten pagina brengt-->
+        <!--    deze knop zorgt dat je alle data naar de resultaten pagina brengt-->
         <input type="submit" value="Resultaat" id="submitknoppie">
     </form>
 </div>
